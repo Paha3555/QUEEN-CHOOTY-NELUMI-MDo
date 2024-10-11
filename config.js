9f0064f0f962b01aee571a70ec0fa17c9c5daa1c
Navinofc44 <Pahasarasanujja@gmail.com>
2024-10-11 19:05:23 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,8 +5,8 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "put session_id",
-MONGODB: process.env.MONGODB || "mongodb+srv://kulathungaasitha319:yjHB0DvFfStNfwPS@cluster0.3oijd.mongodb.net/",
+SESSION_ID: process.env.SESSION_ID || "tZBzXSKY#RcpNZ-6lSHfJv_LDFn7uaX93LXo9Qs6uMZWsC7rP6qo",
+MONGODB: process.env.MONGODB || "mongodb://mongo:HtZTZEYJvqfbOwHfTarmahlqKWzIxteZ@autorack.proxy.rlwy.net:31989",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://8030.us.kg/file/zOm4HoO6YnQR.jpg",
 AUTO_READ_CMD: process.env.AUTO_READ_CMD || "true",
 AUTO_BIO:"true",
